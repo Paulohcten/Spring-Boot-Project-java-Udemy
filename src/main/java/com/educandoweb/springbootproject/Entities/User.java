54e8805b3a2dc88cd01fc746449e62a8b98d2dbc
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-04 16:51:52 -0300

    Service layer, component Registration

--- src/main/java/com/educandoweb/springbootproject/Entities/User.java
+++ src/main/java/com/educandoweb/springbootproject/Entities/User.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Entity
 public class User implements Serializable {
@@ -70,27 +71,15 @@ public class User implements Serializable {
     }
 
     @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return id.equals(user.id);
     }
 
     @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        User other = (User) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
--- src/main/java/com/educandoweb/springbootproject/Resources/UserResource.java
+++ src/main/java/com/educandoweb/springbootproject/Resources/UserResource.java
@@ -2,18 +2,33 @@ package com.educandoweb.springbootproject.Resources;
 
 import com.educandoweb.springbootproject.Entities.User;
 
+import com.educandoweb.springbootproject.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/Users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User user = new User(1L,"Jack Black","jack@gmail.com","999999999","12345");
-        return ResponseEntity.ok().body(user);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 }
