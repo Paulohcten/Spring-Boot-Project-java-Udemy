eda6ba12d0979a55463956a13a4ec0de2e5a4db1
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-05 10:53:36 -0300

    Implemented enumered clas 'OrderStatus

--- src/main/java/com/educandoweb/springbootproject/Config/TestConfig.java
+++ src/main/java/com/educandoweb/springbootproject/Config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.springbootproject.Config;
 
+import com.educandoweb.springbootproject.Entities.Enums.OrderStatus;
 import com.educandoweb.springbootproject.Entities.Orders;
 import com.educandoweb.springbootproject.Entities.User;
 import com.educandoweb.springbootproject.Repositories.OrderRepository;
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Carlinhos Brown", "carlos@gmail.com", "988888889", "123456");
         User u2 = new User(null, "Bono Vox", "bono@gmail.com", "977777779", "123456");
 
-        Orders o1 = new Orders(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Orders o2 = new Orders(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Orders o3 = new Orders(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Orders o1 = new Orders(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Orders o2 = new Orders(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Orders o3 = new Orders(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1,o2,o3));
