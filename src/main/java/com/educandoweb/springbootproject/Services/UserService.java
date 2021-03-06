5b7f319634dac024dc94bfb5a1e163222e33eeb2
Paulo Henrique COngentino Ten <phcten@gmail.com>
2021-03-06 12:45:09 -0300

    Exception Handlin: findById

--- src/main/java/com/educandoweb/springbootproject/Services/UserService.java
+++ src/main/java/com/educandoweb/springbootproject/Services/UserService.java
@@ -2,6 +2,7 @@ package com.educandoweb.springbootproject.Services;
 
 import com.educandoweb.springbootproject.Entities.User;
 import com.educandoweb.springbootproject.Repositories.UserRepository;
+import com.educandoweb.springbootproject.Services.Exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService{
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(()-> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
